4cc4722479b988fb2aa0335aac101ec8bda90299
Imene Fadhlaoui <imenefadhlaoui19@gmail.com>
2026-04-19 15:34:13 +0100

    feat: add auth service with BehaviorSubject, update navbar with user state

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,10 @@ const swaggerSpec = require('./config/swagger');
 const bookingRoutes = require('./routes/booking.routes');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true
+}));
 app.use(cookieP());
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
--- frontend/src/app/shared/navbar/navbar.ts
+++ frontend/src/app/shared/navbar/navbar.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class NavbarComponent {
+  
+  private authService = inject(AuthService);
 
+  currentUser$ = this.authService.currentUser$;
+
+
+  onLogout(): void {
+    this.authService.logout().subscribe();
+  }
 }
